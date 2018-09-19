d1579ba87a8623dc68d8f7ea55971d03a28c90f3
william ermlcik <ermlickw@vt.edu>
2018-09-19 16:54:07 -0400

    'final'

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -9,20 +9,20 @@ Eliza will begin the dialogue by asking the name of the user. Current implementa
 
 1)	"Word Spotting" function to extract the keywords from users' answer and give related feedback.
     (e.g. 'sad' to 'What makes you sad?')
-2)	 Transforming Sentences from user to the answers/questions from Eliza.(e.g " I love you." to "You love me?")
+2)	 Transforming Sentences from user to the answers/questions from Eliza.(e.g " I am not sure what to do." to "You are not sure what to do?")
 3)	Personalized the conversation by using user name in some of the questions
 4)	Robust answers in some plausible way when the users enters some gibberish sentence or the sentences are not understandable
     (e.g.I DIDN'T QUITE UNDERSTAND. CAN YOU SAY THAT ANOTHER WAY?)
 5)	Used lambda function for asking default questions to the user, such as storing and returning the user's favorite things throughout discourse.
 
 ***********************HOW TO RUN THE PROGRAM:****************************************************************************************************
-When using the code, just run the Assignment1.py and type your response into the prompt and press ENTER. Do not use contractions.
+When using the code, run the Assignment1.py, type your response into the prompt and press ENTER. Do not use contractions in your reply.
 
 ***********************HOW TO END THE PROGRAM:****************************************************************************************************
 Program can be ended by using any of these keywords
-•	Bye
-•	Farewell
-•	Adios
+•	bye
+•	farewell
+•	adios
 •	see you later
 •	talk to you later
 
@@ -32,8 +32,8 @@ SIMPLE EXAMPLE DIALOGUE:
 Simple Example Dialogue:
 HELLO, MY NAME IS ELIZA. I AM A PSYCHOTHERAPIST. WHAT IS YOUR NAME?
     'My name is Coco'
-NICE TO MEET YOU, COCO. HOW DO YOU FEEL TODAY? 
-    'i fee l sAD...'    
+NICE TO MEET YOU, COCO. HOW DO YOU FEEL TODAY?
+    'i fee l sAD...'
 "I DIDN'T QUITE UNDERSTAND. CAN YOU SAY THAT ANOTHER WAY?"
     'I feel sad'
 TELL ME MORE.
@@ -42,17 +42,17 @@ CAN YOU THINK OF A SPECIFIC EXAMPLE?
     'Yes, I need to read 10 paper a week'
 ARE YOU SURE?
     'Pretty sure'
-COCO, WHY PRETTY SURE?     
+COCO, WHY PRETTY SURE?
     'can you sing a song for me?'
 Sure
     'I want to share my favorites'
-COCO, WHY DO YOU WANT TO SHARE YOUR FAVORITES?    
+COCO, WHY DO YOU WANT TO SHARE YOUR FAVORITES?
     'favorite is an interesting topic'
 SPEAKING OF FAVORITES... I'D LIKE TO LEARN MORE ABOUT YOU
 I know your favorite song is ...well maybe I am not sure....
-...What is your favorite song?  
-    'My favorite song is Raise you up'   
-GOT IT. WHAT ELSE IS ON YOUR MIND?     
+...What is your favorite song?
+    'My favorite song is Raise you up'
+GOT IT. WHAT ELSE IS ON YOUR MIND?
     'Nothing! bye"
 I HOPE THIS CONVERSATION WAS PRODUCTIVE. GOODBYE.
 ***************************************************************************************
